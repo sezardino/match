b81e41df0879600a2664c3a28580906c72080f41
Edward <edward.arechwa@gogomedia.pl>
2021-05-17 09:34:46 +0200

    feat: add popup and register form

--- src/scripts/components/popup.ts
+++ src/scripts/components/popup.ts
@@ -1,4 +1,6 @@
+import render from '../utils/render';
 import Component from './component';
+import RegisterForm from './registerForm';
 
 const popupTemplate = () => `
 <div class="popup">
@@ -7,11 +9,39 @@ const popupTemplate = () => `
 </div>`;
 
 class Popup extends Component {
+    popupClass: string;
+    popupOpenClass: string;
+    popupInnerSelector: string;
+
     constructor() {
         super();
         this.template = popupTemplate();
 
+        this.popupClass = 'popup';
+        this.popupOpenClass = 'popup--open';
+        this.popupInnerSelector = '.popup__inner';
+
         this.onLoad();
+    }
+
+    open(inner: Component | RegisterForm): void {
+        if (inner) {
+            const popupInner = this.element.querySelector(
+                this.popupInnerSelector
+            );
+            this.element.classList.add(this.popupOpenClass);
+            render.renderAB(popupInner, inner);
+        }
+    }
+
+    onLoad() {
+        super.onLoad();
+
+        this.element.addEventListener('click', (evt: MouseEvent) => {
+            if (evt.target.classList.contains(this.popupClass)) {
+                this.element.classList.remove(this.popupOpenClass);
+            }
+        });
     }
 }
 
---
+++ src/scripts/components/registerForm.ts
@@ -0,0 +1,103 @@
+import Component from './component';
+
+import userLogo from '../../assets/svg/user.svg';
+
+const registerFormTemplate = () => `
+<section class="register-user">
+    <h2 class="register-user__title">Register new Player</h2>
+    <form class="register-user__form form form--register">
+        <div class="form__row">
+            <div class="form__wrapper">
+                <label class="form__label form__label--invalid">
+                    <span class="form__label-text"
+                        >First Name</span
+                    >
+                    <input
+                        type="text"
+                        name="name"
+                        class="form__input"
+                    />
+                </label>
+                <label class="form__label form__label--valid">
+                    <span class="form__label-text"
+                        >Last Name</span
+                    >
+                    <input
+                        type="text"
+                        name="surname"
+                        class="form__input"
+                    />
+                </label>
+                <label class="form__label">
+                    <span class="form__label-text">E-mail</span>
+                    <input
+                        type="mail"
+                        name="email"
+                        class="form__input"
+                    />
+                </label>
+            </div>
+            <img
+                src="${userLogo}"
+                width="160"
+                height="160"
+                alt="user image"
+                class="form__img"
+            />
+        </div>
+        <footer class="form__footer">
+            <button
+                class="form__button form__submit button button--add button--uppercase"
+            >
+                Add user
+            </button>
+            <button
+                class="form__button form__cancel button button--primary button--uppercase"
+            >
+                Cancel
+            </button>
+        </footer>
+    </form>
+</section>
+`;
+
+class RegisterForm extends Component {
+    submitButtonSelector: string;
+    cancelButtonSelector: string;
+    form: HTMLFormElement;
+    cancelButton: HTMLButtonElement;
+
+    constructor() {
+        super();
+        this.template = registerFormTemplate();
+        this.cancelButtonSelector = '.form__cancel';
+
+        this.onLoad();
+    }
+
+    submitHandler(handler: () => void): void {
+        this.form.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+            handler();
+        });
+    }
+
+    cancelHandler(handler: () => void): void {
+        this.cancelButton.addEventListener('click', (evt) => {
+            evt.preventDefault();
+            handler();
+        });
+    }
+
+    onLoad() {
+        super.onLoad();
+
+        this.form = this.element.querySelector('form');
+
+        this.cancelButton = this.element.querySelector(
+            this.cancelButtonSelector
+        );
+    }
+}
+
+export default RegisterForm;
